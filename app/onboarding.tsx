01b7e765b9126db6ee04a196995eea390916cfc5
Kapil Motwani <kapilmotwani1010@gmail.com>
2026-04-05 22:57:09 +0530

    tab navigation working and search input added and search input is redirecting to search screen.

---
+++ app/onboarding.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  );
+};
+export default Onboarding;
+const styles = StyleSheet.create({});
